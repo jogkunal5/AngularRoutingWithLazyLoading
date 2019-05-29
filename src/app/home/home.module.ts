3ef7128a3cf9a622ce6f8a3723a6ead7af638756
jogkunal5 <jogkunal5@gmail.com>
2019-05-29 19:46:24 +0530

    Created child components for home and index

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent, SettingsComponent, ProductsComponent],
   imports: [
     CommonModule
   ]
--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotComponent } from './forgot/forgot.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, SignupComponent, ForgotComponent],
   imports: [
     CommonModule
   ]
